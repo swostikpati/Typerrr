0547897f1e5a472c5d81c4ce684b07a1e114132a
Swostik Pati <swostikpati@gmail.com>
2022-11-04 19:54:31 +0400

    added additional checks for starting race

--- public/page1/app.js
+++ public/page1/app.js
@@ -1,5 +1,11 @@
 let socket = io(); //establishes connection with the socket server
 
+//variables
+// let roomFull = false;
+
+//DOM elements
+const start_bt = document.querySelector("#start-bt");
+const pre_start = document.querySelector(".pre-start");
 //client acknowledging after server confirmation - connect is a keyword
 socket.on("connect", () => {
     console.log("Connection established to server via sockets");
@@ -7,5 +13,19 @@ socket.on("connect", () => {
         console.log("Room joined:", data);
     })
 
+})
+
+socket.on("roomFull", () => {
+    // roomFull = data;
+    console.log("yes");
+    start_bt.disabled = false;
+})
+
+socket.on("startRace", () => {
+    pre_start.style.display = "none";
+})
+
+start_bt.addEventListener("click", () => {
+    socket.emit("raceStarted");
 })
 
